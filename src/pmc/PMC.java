66b1b0099fc6350016dd955cef2e36a4236d8818
carlasanches <carlasanches8@gmail.com>
2020-10-16 14:57:22 -0300

    Classe Item
    Classe dos itens que serão armazenados na mochila. Cada item possui um peso e um lucro.

--- src/pmc/PMC.java
+++ src/pmc/PMC.java
@@ -5,6 +5,9 @@
  */
 package pmc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Carla
@@ -16,6 +19,26 @@ public class PMC {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+        
+        Item item1 = new Item(10,3);
+        Item item2 = new Item(8,4);
+        Item item3 = new Item(4,4);
+        Item item4 = new Item(6,5);
+        Item item5 = new Item(8,7);
+        Item item6 = new Item(6,9);
+        Item item7 = new Item(9,9);
+        Item item8 = new Item(6,10);
+        
+        List<Item> itens = new ArrayList();   
+        itens.add(item1);
+        itens.add(item2);
+        itens.add(item3);
+        itens.add(item4);
+        itens.add(item5);
+        itens.add(item6);
+        itens.add(item7);
+        itens.add(item8);
+
+        itens.forEach((item)->System.out.println(item.toString()));
+    }    
 }
